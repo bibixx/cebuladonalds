d3d8c67cb0f763d673f783701e140c42050cd129
Bartosz Legięć <bartosz@legiec.eu>
2019-07-08 18:43:18 +0200

    Add simple working version

---
+++ steps.js
@@ -0,0 +1,57 @@
+// var coupon = "0e82-yzia-mmpi".split("-")
+// var couponInputs = $$('input[type="text"]');
+
+// couponInputs[0].input(coupon[0]);
+// couponInputs[1].input(coupon[1]);
+// couponInputs[2].input(coupon[2]);
+
+// Wait for "Czy ma Pan(i) przynajmniej 15 lat?"
+// Click on "Tak"
+
+// Wait for "Biorąc pod uwagę Pana(i) wizytę w restauracji McDonald’s w dniu 08/07/2019 w jaki sposób złożył(a) Pan(i) zamówienie?"
+// Click on "W kasie samoobsługowej/ kiosku/ przy ekranie dotykowym"
+
+// Wait for "Czy zamówił(a) Pan(i) posiłek"
+// Click on "Do zjedzenia na miejscu/ w restauracji"
+
+// Wait for "W jaki sposób odebrał(a) Pan(i) zamówienie?"
+// Click on "Przy ladzie"
+
+// Wait for "W jakim stopniu był(a) Pan(i) zadowolony/a z wizyty w ocenianej restauracji McDonald’s w dniu 08/07/2019?"
+// Click on "Bardzo zadowolony/a"
+
+// Wait for "Co zadecydowało, że był(a) Pan(i) Bardzo zadowolony/a z ocenianej wizyty w McDonald’s w dniu 08/07/2019?"
+// Write to textarea "a b c d e"
+
+// Wait for "W jakim stopniu był(a) Pan(i) zadowolony/a z następujących aspektów tej wizyty?"
+// Click on $$('.rating_prompt .option:first-child input[type="radio"')
+
+// Wait for "Czy odwiedzając McDonald’s zauważył(a) Pan(i) pracownika restauracji, który znajdował się w holu lub sali jadalnej i witał/żegnał gości, rozmawiał z nimi, pomagał, kiedy było potrzeba?"
+// Click on "Nie"
+
+// Wait for "Czy produkty, które Pan(i) zamówił(a) były przygotowane prawidłowo?"
+// Click on "Tak"
+
+// Wait for "Czy Pana/i zamówienie było prawidłowo skompletowane?"
+// Click on "Tak"
+
+// Wait for "Czy odwiedzi Pan(i) ocenianą restaurację McDonald’s w ciągu najbliższych 30 dni?"
+// Click on "Zdecydowanie tak"
+
+// Wait fot "W skali od 0 do 10, na ile prawdopodobne jest, że polecił(a)by Pan(i) ocenianą restaurację McDonald’s znajomemu lub członkowi rodziny?"
+// Click on $("input[type='radio']")[0]
+
+// Wait for "Płeć"
+// Click on "Mężczyzna"
+// Input into $("input[type='text']") 21
+
+// Wait for "Liczba osób towarzyszących Panu/i podczas wizyty w ocenianej restauracji McDonald’s:"
+// Click on "Byłem/am sam(a)"
+
+// Wait for "Liczba osób towarzyszących Panu/i podczas wizyty w ocenianej restauracji McDonald’s:"
+// Click on "Byłem/am sam(a)"
+
+// Wait for "Dziękujemy za udział w badaniu. Jako forma podziękowania oferujemy Panu/i kupon z kodem na hamburgera lub na duże lody z polewą. Proszę wybrać jeden kupon."
+// $("input[type='radio']") – 0: 🍔 1: 🍦
+
+// Click on "Dalej"
